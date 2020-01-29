6f0d8aa052f412172e842d01fd2922972da267aa
Julian Commissaris <jpcommissaris@gmail.com>
2020-01-28 16:03:43 -0800

    finished both

--- Arithmetic.asm
+++ Arithmetic.asm
@@ -1,14 +1,38 @@
-# Arithmetic.asm
-# A simple calculator program in MIPS Assembly for CS64
-#
 
 .text
+
 main:
 
-	# TODO: Write your code here
+	# prompt for int
+	li $v0, 5
+	syscall
+	move $t0, $v0
+
+	# prompt for int
+	li $v0, 5
+	syscall
+	move $t1, $v0
+
+	# prompt for int
+	li $v0, 5
+	syscall
+	move $t2, $v0
+	
+	# do math
+	add $t3, $t0, $t1
+	sll $t3, $t3, 2
+	li $t0, 5
+	mult $t2, $t0
+	mflo $t4
+	sub $t5, $t3, $t4
+
+	# display
+	li $v0, 1
+	move $a0, $t5
+	syscall
+
 
 exit:
 	# Exit
 	li $v0, 10
 	syscall
-
--- hello.asm
+++ hello.asm
@@ -1,18 +1,44 @@
-# hello.asm
-# A "Hello World" program in MIPS Assembly for CS64
-#
-#  Data Area - allocate and initialize variables
+
 .data
-	# TODO: Write your string definitions here
+	prompt: .asciiz "Choose an integer number between 0 and 1000:\n"
+	str1: .asciiz "Hello World!\n"
+	str2: .asciiz "The magic number is "
+	str3: .asciiz "!!!\n"
 
-#Text Area (i.e. instructions)
 .text
-main:
 
-	# TODO: Write your code here
+main:
+	# print the prompt 
+	li $v0, 4
+	la $a0, prompt
+	syscall
+
+	# prompt for int 
+	li $v0, 5
+	syscall
+	move $t0, $v0
+
+	# print hello world
+	li $v0, 4
+	la $a0, str1
+	syscall
+
+	# print numer string
+	li $v0, 4
+	la $a0, str2
+	syscall
+
+	# print number and !!
+	li $v0, 1
+	move $a0, $t0 
+	syscall
+	li $v0, 4
+	la $a0, str3
+	syscall
 
 exit:
 	# Exit
 	li $v0, 10
 	syscall
+
 
